2bd69b4953b7c5063728814a5eeaec4d7250e771
superbobr <leonseoprofi@gmail.com>
2020-05-16 14:59:48 +0300

    Initial commit

---
+++ test.php
@@ -0,0 +1,14 @@
+<?php
+
+var_dump(!1);//false
+var_dump(!0);//true
+var_dump(!true);//false
+var_dump(2 && 3);//true
+var_dump(5 && 0);//false
+var_dump(3 || 0);//true
+var_dump(5 / 1);//5
+var_dump(1 / 5);//0.2
+var_dump(5 + '5string');//10
+var_dump('5' == 5);//true
+var_dump('05' == 5);//true
+var_dump('05' == '5');//true
